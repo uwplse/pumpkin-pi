8d8fd258a5996385779b920ef4c55bac65195e77
Talia Ringer <tringer@warfa.cs.washington.edu>
2018-02-21 17:07:44 -0800

    Prove my code is correct for append, but it's gross

--- plugin/coq/Apply.v
+++ plugin/coq/Apply.v
@@ -13,9 +13,27 @@ Definition hd (A : Type) (default : A) (l : list A) :=
 
 Apply ornament orn_list_vector orn_list_vector_inv in hd as hd_vect_auto.
 
-Print hd_vect_auto.
+Definition hd_vect (A : Type) (default : A) (n : nat) (v : vector A n) :=
+  vector_rect
+    A
+    (fun (n0 : nat) (_ : vector A n0) => A)
+    default
+    (fun (n0 : nat) (x : A) (_ : vector A n0) (_ : A) =>
+      x)
+    n
+    v.
 
-(* TODO test *)
+(*
+ * Note how it's not definitionally equal, but we can prove it.
+ * For it to be definitionally equal, we need to internalize the
+ * ornamentation and run some reduction steps.
+ *)
+Theorem test_orn_hd :
+  forall (A : Type) (a : A) (n : nat) (v : vector A n),
+    hd_vect_auto A a n v = hd_vect A a n v.
+Proof.
+  intros. induction v; auto.
+Qed.
 
 Definition append (A : Type) (l1 : list A) (l2 : list A) :=
   list_rect
@@ -25,10 +43,47 @@ Definition append (A : Type) (l1 : list A) (l2 : list A) :=
       a :: IH)
     l1.
 
+Definition append_vect (A : Type) (n1 : nat) (v1 : vector A n1) (n2 : nat) (v2 : vector A n2) :=
+  vector_rect
+    A
+    (fun (n0 : nat) (_ : vector A n0) => vector A (n0 + n2))
+    v2
+    (fun (n0 : nat) (a : A) (_ : vector A n0) (IH : vector A (n0 + n2)) =>
+      consV A (n0 + n2) a IH)
+    n1
+    v1.
+
 Apply ornament orn_list_vector orn_list_vector_inv in append as append_vect_auto.
 
-Print append_vect_auto.
+(*
+ * For this one, we can't state the equality, but we can use existsT.
+ *)
+Definition eq_vect A n (v : vector A n) n' (v' : vector A n') :=
+  existT (vector A) n v = existT (vector A) n' v'.
 
-(* TODO test *)
+Theorem eq_vect_cons:
+  forall A n (v : vector A n) n' (v' : vector A n'),
+    eq_vect A n v n' v' ->
+    forall (a : A), eq_vect A (S n) (consV A n a v) (S n') (consV A n' a v').
+Proof.
+  unfold eq_vect.
+  intros. inversion H. subst. auto.
+Qed.
+
+Theorem test_orn_append:
+  forall A n (v : vector A n) n' (v' : vector A n'),
+    eq_vect
+      A
+      (n + n')
+      (append_vect A n v n' v')
+      (orn_list_vector_index
+        A
+        (append A (orn_list_vector_inv A n v) (orn_list_vector_inv A n' v')))
+      (append_vect_auto A n v n' v').
+Proof.
+  unfold eq_vect.
+  intros. induction v; induction v'; try apply eq_vect_cons; auto.
+Qed.
+
 
 
