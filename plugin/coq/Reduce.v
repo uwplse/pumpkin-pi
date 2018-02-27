c2f5257e251ce432a88b80810ca26bc19bed2cf8
Talia Ringer <tringer@warfa.cs.washington.edu>
2018-02-27 14:37:16 -0800

    test reduction on head

--- plugin/coq/Reduce.v
+++ plugin/coq/Reduce.v
@@ -6,4 +6,15 @@ Require Import Apply.
 
 (* --- Simple functions on lists --- *)
 
-Reduce ornament orn_list_vector orn_list_vector_inv in hd_vect_auto as hd_vect.
+Reduce ornament orn_list_vector orn_list_vector_inv in hd_vect_auto as hd_vect_red.
+
+Theorem test_hd_vect:
+  forall (A : Type) (default : A) (n : nat) (v : vector A n),
+    hd_vect A default n v = hd_vect_red A default n v.
+Proof.
+  intros. reflexivity.
+Qed.
+
+(* TODO app *)
+
+(* TODO deorn *)
