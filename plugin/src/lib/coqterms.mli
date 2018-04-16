5bd69e5269e271a9c61d5bfec2faa452f5b4ad8a
Talia Ringer <tringer@quarian.dyn.cs.washington.edu>
2018-04-16 13:16:45 -0700

    zooming and reconstructing

--- plugin/src/lib/coqterms.mli
+++ plugin/src/lib/coqterms.mli
@@ -125,3 +125,36 @@ val infer_type : env -> evar_map -> types -> types
  * Convertibility, ignoring universe constraints
  *)
 val convertible : env -> types -> types -> bool
+
+(* --- Zooming and reconstructing --- *)
+                    
+(*
+ * Zoom n deep
+ *)
+val zoom_n_prod : env -> int -> types -> (env * types)
+val zoom_n_lambda : env -> int -> types -> (env * types)
+
+(*
+ * Zoom all the way
+ *)
+val zoom_lambda_term : env -> types -> (env * types)
+val zoom_product_type : env -> types -> (env * types)
+
+(* 
+ * Projections of zooming
+ *)
+val zoom_env : (env -> types -> (env * types)) -> env -> types -> env
+val zoom_term : (env -> types -> (env * types)) -> env -> types -> types
+
+(* 
+ * Reconstruct until n are left
+ *)
+val reconstruct_lambda_n : env -> types -> int -> types
+val reconstruct_product_n : env -> types -> int -> types
+
+(* 
+ * Reconstruct fully
+ *)
+val reconstruct_lambda : env -> types -> types
+val reconstruct_product : env -> types -> types
+
