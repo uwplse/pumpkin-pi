c42286418dc33b4b7d3f2626db91bc52e4ba547a
Talia Ringer <tringer@quarian.dyn.cs.washington.edu>
2018-04-19 16:47:17 -0700

    move all but the top level out of ornamental

--- plugin/src/components/specialization.mli
+++ plugin/src/components/specialization.mli
@@ -6,8 +6,25 @@ open Term
 open Environ
 open Evd
 open Lifting
+open Names
 
 (*
  * Apply an indexing ornament, but don't reduce the result
  *)
-val apply_indexing_ornament : env -> evar_map -> lifting -> types -> types
+val apply_indexing_ornament :
+  env ->
+  evar_map ->
+  lifting -> (* lifting configuration *)
+  types -> (* term to lift *)
+  types (* applied term *)
+
+(*
+ * Reduce an application of an indexing ornament
+ *)
+val internalize :
+  env ->
+  evar_map ->
+  Id.t -> (* name of indexer to generate, if applicable *)
+  lifting -> (* lifting configuration *)
+  types -> (* term to reduce *)
+  (types * types option) (* reduced term and optional indexer *)
