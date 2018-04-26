d6ad3a8bea7baa0feeeabcdfe9c24367669b3952
Talia Ringer <tringer@quarian.dyn.cs.washington.edu>
2018-04-26 12:09:27 -0700

    remove old comment

--- plugin/coq/HigherLift.v
+++ plugin/coq/HigherLift.v
@@ -15,17 +15,13 @@ Higher lift orn_list_vector_inv orn_list_vector in app_nil_r_red along append_ve
 (*
  * These will fail until it's actually implemented
  *)
+
 Theorem test_app_nil_r_vect:
   forall (A : Type) (pv : packed_vector A),
     append_vect_red A pv (existT (vector A) 0 (nilV A)) = pv.
 Proof.
   exact app_nil_r_vect_red_higher.
 Qed.
-
-(* 
- * NOTE: We can simplify the above term at some point, which will give more clarity on
- * the lifted term we're looking for.
- *)
 
 Theorem test_app_nil_r:
   forall (A : Type) (l : list A),
