f19beb0c0df6501cc9d48c80b5f643be134f55e2
Talia Ringer <tringer@quarian.dyn.cs.washington.edu>
2018-05-04 16:18:32 -0700

    test in_split_vect

--- plugin/coq/HigherLift.v
+++ plugin/coq/HigherLift.v
@@ -76,16 +76,38 @@ Higher lift orn_list_vector orn_list_vector_inv in in_split_vect_red as in_split
 Print in_split_vect_higher.
 
 (*
- * TODO in_split_vect is missing one application of its last argument, missing from reduction step.
+ * TODO note how the type still doesn't look even as nice as the one we state below,
+ * even though it indeed has that type.
  *)
-
-(*
 
 Theorem test_in_split_vect_exact:
   forall (A : Type) (x : A) (pv : sigT (vector A)),
+    In_vect_red A x (existT (vector A) (projT1 pv) (projT2 pv)) ->
+       exists pv1 pv2 : sigT (vector A),
+         existT (vector A) (projT1 pv) (projT2 pv) =
+         append_vect_red A pv1
+           (existT (vector A) (S (projT1 pv2)) (consV A (projT1 pv2) x (projT2 pv2))).
+Proof.
+  exact in_split_vect_higher.
+Qed.
+
+Theorem test_in_split_vect:
+  forall (A : Type) (x : A) (pv : sigT (vector A)),
     In_vect_red A x pv ->
-    In_vect_red A x (existT (fun H0 : nat => vector A H0) (projT1 pv) (projT2 pv)) ->
-       exists l1 l2 : {H : nat & vector A H},
-         existT (fun H0 : nat => vector A H0) (projT1 l) (projT2 l) =
-         append_vect_red A l1
-           (existT (fun H : nat => vector A H) (S (projT1 l2)) (consV A (projT1 l2) x (projT2 l2)))*)
+      exists pv1 pv2 : sigT (vector A),
+        pv = 
+        append_vect_red A pv1 
+          (existT (vector A) (S (projT1 pv2)) (consV A (projT1 pv2) x (projT2 pv2))).
+Proof.
+  intros A x pv.
+  rewrite (conv nat (vector A) pv).
+  intros. 
+  apply in_split_vect_higher.
+  apply H. 
+Qed.
+
+(*
+ * Note the above is still predictable enough to derive, which is very good 
+ *)
+
+
