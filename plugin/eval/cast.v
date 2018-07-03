9c2ce77e4091a6633f67ac1fa1a22f9af4ada42e
Nate Yazdani <nyazdani@cs.washington.edu>
2018-07-02 18:21:38 -0700

    Improve utility library for dependent casts

--- plugin/eval/cast.v
+++ plugin/eval/cast.v
@@ -14,26 +14,45 @@ Notation decType := Decidable.t.
 Notation eq_dec := (Decidable.eq_dec _).
 Coercion Decidable.type : decType >-> Sortclass.
 
-(* A synonym for eq_rect with implicit inference of more arguments. *)
-Definition cast {A : Type} {P : A -> Type} {x y : A} (p : P x) (E : x = y) : P y :=
-  @eq_rect A x P p y E.
+Notation UIP A := (@Eqdep_dec.UIP_dec A eq_dec _ _ _ eq_refl).
+
+Notation cast p E := (@eq_rect _ _ _ p _ E).
 
 Definition cast_trivial {A : decType} {P : A -> Type} {x : A}
-           (p : P x) (E : x = x) : @cast A P x x p E = p :=
-  eq_sym (@Eqdep_dec.eq_rect_eq_dec A eq_dec x P p E).
+           (p : P x) (E : x = x) : cast p E = p.
+  rewrite (UIP A). reflexivity.
+Defined.
+
+(* Definition cast_trivial {A : decType} {P : A -> Type} {x : A} *)
+(*            (p : P x) (E : x = x) : cast p E = p := *)
+(*   eq_sym (@Eqdep_dec.eq_rect_eq_dec A eq_dec x P p E). *)
 
 Program Canonical Structure nat_decType :=
   Decidable.Pack nat _.
 Next Obligation. decide equality. Defined.
 
-Lemma cast_struct {A : decType} {B : A -> Type} {x y : A}
-      {g : A -> A} {inj : forall x y, g x = g y -> x = y}
-      (E : g x = g y) (f : forall x, B x -> B (g x))
-      (p : B x) : cast (f x p) E = f y (cast p (inj x y E)).
+Lemma cast_sigma {A : Type} {B : A -> Type} {C : Type} {x y : A}
+      (f : forall x, B x -> C) (E : x = y)
+      (p : B x) : f y (cast p E) = f x p.
 Proof.
-  assert (E' := (inj x y E)). revert p E. rewrite E'. intros p E.
-  repeat rewrite cast_trivial. reflexivity.
+  revert p. rewrite <- E. intro p. reflexivity.
 Defined.
+
+Lemma cast_functor {A : decType} {B C : A -> Type} {g : A -> A} {x y : A}
+      (f : forall x, B x -> C (g x)) (E : x = y) (E' : g x = g y)
+      (p : B x) : cast (f x p) E' = f y (cast p E).
+Proof.
+  revert p E'. rewrite <- E. intros p E'. rewrite cast_trivial. reflexivity.
+Defined.
+
+(* Lemma cast_struct {A : decType} {B : A -> Type} {x y : A} *)
+(*       {g : A -> A} {inj : forall x y, g x = g y -> x = y} *)
+(*       (E : g x = g y) (f : forall x, B x -> B (g x)) *)
+(*       (p : B x) : cast (f x p) E = f y (cast p (inj x y E)). *)
+(* Proof. *)
+(*   assert (E' := (inj x y E)). revert p E. rewrite E'. intros p E. *)
+(*   repeat rewrite cast_trivial. reflexivity. *)
+(* Defined. *)
 
 Lemma cast_contract {A : decType} {B : A -> Type}
       {f : forall x, B x} {x y : A} (E : x = y) : cast (f x) E = f y.
@@ -68,39 +87,39 @@ Canonical Structure add_comm (n m : nat) :=
   Identity.Pack nat (n + m) (m + n) (add_comm n m).
 (* FIXME: Inference of canonical instances is too shallow; we really need first-order unification. *)
 
-Module Vector.
+(* Module Vector. *)
 
-Inductive vector (A : Type) : nat -> Type :=
-| vcons (n : nat) (x : A) : vector A n -> vector A (S n)
-| vnil : vector A O.
-Arguments vcons {A n}.
-Arguments vnil {A}.
+(* Inductive vector (A : Type) : nat -> Type := *)
+(* | vcons (n : nat) (x : A) : vector A n -> vector A (S n) *)
+(* | vnil : vector A O. *)
+(* Arguments vcons {A n}. *)
+(* Arguments vnil {A}. *)
 
-Fixpoint vapp {A n m} (xs : vector A n) (ys : vector A m) : vector A (n + m) :=
-  match xs with
-  | vcons x xs => vcons x (vapp xs ys)
-  | vnil => ys
-  end.
+(* Fixpoint vapp {A n m} (xs : vector A n) (ys : vector A m) : vector A (n + m) := *)
+(*   match xs with *)
+(*   | vcons x xs => vcons x (vapp xs ys) *)
+(*   | vnil => ys *)
+(*   end. *)
 
-Lemma cast_vcons {A n m} (x : A) (xs : vector A n) (E : S n = S m) :
-    cast (vcons x xs) E = vcons x (cast xs (eq_add_S n m E)).
-Proof.
-  apply (cast_struct E (fun n => @vcons A n x) xs).
-Defined.
+(* Lemma cast_vcons {A n m} (x : A) (xs : vector A n) (E : S n = S m) : *)
+(*     cast (vcons x xs) E = vcons x (cast xs (eq_add_S n m E)). *)
+(* Proof. *)
+(*   apply (cast_struct E (fun n => @vcons A n x) xs). *)
+(* Defined. *)
 
-Definition bitvector := vector bool.
-Definition bv1010 : bitvector 4 :=
-  (vcons true (vcons false (vcons true (vcons false vnil)))).
+(* Definition bitvector := vector bool. *)
+(* Definition bv1010 : bitvector 4 := *)
+(*   (vcons true (vcons false (vcons true (vcons false vnil)))). *)
 
-Check (cast bv1010 (plus_n_O 4)).
+(* Check (cast bv1010 (plus_n_O 4)). *)
 
-Lemma vapp_vnil_r {A n} (xs : vector A n) :
-  <| vapp xs vnil |> = xs.
-Proof.
-  simpl. generalize (eq_sym (plus_n_O n)) as E.
-  induction xs; simpl; intros E.
-  - rewrite cast_vcons, IHxs. reflexivity.
-  - rewrite cast_trivial. reflexivity.
-Qed.
+(* Lemma vapp_vnil_r {A n} (xs : vector A n) : *)
+(*   <| vapp xs vnil |> = xs. *)
+(* Proof. *)
+(*   simpl. generalize (eq_sym (plus_n_O n)) as E. *)
+(*   induction xs; simpl; intros E. *)
+(*   - rewrite cast_vcons, IHxs. reflexivity. *)
+(*   - rewrite cast_trivial. reflexivity. *)
+(* Qed. *)
 
-End Vector.
+(* End Vector. *)
