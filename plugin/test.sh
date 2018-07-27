8a0b9e8880347f56249cd0d1ec8d686fb05f12b8
Talia Ringer <tringer@quarian.dyn.cs.washington.edu>
2018-07-26 22:29:44 -0700

    create directories if they don't exist

--- plugin/test.sh
+++ plugin/test.sh
@@ -2,6 +2,20 @@
 
 coqc coq/Test.v
 coqc coq/Lift.v
+
 cd eval
+
+if [ -e out ]
+then
+  rm -r out
+else
+  :
+fi
+
+mkdir out
+mkdir out/inorder
+mkdir out/postorder
+mkdir out/preorder
+mkdir out/search
 coqc main.v
 cd ..
