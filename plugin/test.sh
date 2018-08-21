475553b457de82cd54f211abac59675f8675f21e
Talia Ringer <tringer@quarian.dyn.cs.washington.edu>
2018-08-21 09:09:18 -0700

    add error messaging for failed lifting

--- plugin/test.sh
+++ plugin/test.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-coqc coq/Test.v
-coqc coq/Lift.v
-coqc coq/Indtype.v
+lifted=false
+liftedind=false
+liftedcase=false
+
+if coqc coq/Test.v 
+then
+  :
+else
+  echo "ERROR: Searching for ornaments failed"
+  exit 1
+fi
+
+if coqc coq/Lift.v 
+then
+  lifted=true
+else
+  :
+fi
+
+if coqc coq/Indtype.v 
+then
+  liftedind=true
+else
+  :
+fi
 
 cd eval
 
@@ -19,5 +41,41 @@ mkdir out/postorder
 mkdir out/preorder
 mkdir out/search
 mkdir out/normalized
-make clean && make
+make clean
+if make
+then
+  liftedcase=true
+else
+  :
+fi
 cd ..
+
+if [ $lifted = true ] && [ $liftedind = true ] && [ $liftedcase = true ]
+then
+  echo "SUCCESS: All tests passed."
+elif [ $lifted = true ] && [ $liftedcase = true ]
+then
+  echo "WARNING: All POPL tests passed, but lifted inductive predicates are broken. See Coq error message."
+else
+  echo "ERROR: The following tests failed, including POPL tests:"
+  if [ !$lifted = true ]
+  then
+    echo "lifting"
+  else
+    :
+  fi
+  if [ !$liftedind = true ]
+  then
+    echo "lifting inductive predicates"
+  else
+    :
+  fi
+  if [ !$liftedcase = true ]
+  then
+    echo "case study code"
+  else
+    :
+  fi
+  echo "See Coq error message."
+fi
+  
