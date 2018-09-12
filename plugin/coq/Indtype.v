1ab2eeb2f8249128d92ed0e3a909097e913e10fc
Nate Yazdani <nyazdani@cs.washington.edu>
2018-09-12 14:14:48 -0700

    Manual examples of lifting-compatible eliminators

--- plugin/coq/Indtype.v
+++ plugin/coq/Indtype.v
@@ -3,9 +3,10 @@ Require Import Ornamental.Ornaments.
 Require Import List Sorting.Permutation.
 Require Import Test Lift.
 
+Notation "( x ; y )" := (existT _ x y) (no associativity).
 Notation "p .1" := (projT1 p) (left associativity, at level 8).
 Notation "p .2" := (projT2 p) (left associativity, at level 8).
-Notation "( x ; y )" := (existT _ x y) (no associativity).
+Notation "p .&" := (p.1; p.2) (left associativity, at level 6).
 
 Notation hdV := hd_vect_lifted.
 Notation tlV := tl_vect_lifted.
@@ -16,7 +17,6 @@ Definition length {A : Type} (xs : list A) : nat :=
     O
     (fun _ _ n => S n)
     xs.
-
 Lift list vector in @length as lengthV.
 
 (* Ex. 1: Promoting append relation from lists to vectors *)
@@ -28,8 +28,67 @@ Section Append.
       is_app (x :: xs) ys (x :: zs)
   | is_app_nil (ys : list A) :
       is_app nil ys ys.
-
   Lift list vector in @is_app as ..V.
+
+  Fixpoint is_appV_rect_sig
+           (A : Type)
+           (P :
+              forall (xs ys zs : {n:nat & vector A n}),
+                is_appV A xs.& ys.& zs.& -> Type)
+           (P_cons :
+              forall (x : A) (xs ys zs : {n:nat & vector A n})
+                     (tl : is_appV A xs.& ys.& zs.&),
+                P xs.& ys.& zs.& tl ->
+                P (S xs.1; consV A xs.1 x xs.2)
+                  ys.&
+                  (S zs.1; consV A zs.1 x zs.2)
+                  (is_app_consV A x xs.& ys.& zs.& tl))
+           (P_nil :
+              forall (ys : {n:nat & vector A n}),
+                P (O; nilV A) ys.& ys.& (is_app_nilV A ys))
+           (xs ys zs : {n:nat & vector A n})
+           (pf : is_appV A xs.& ys.& zs.&) {struct pf} :
+    P xs.& ys.& zs.& pf :=
+    match pf as pf in is_appV _ ((_; _) as xs) ((_; _) as ys) ((_; _) as zs) return P (xs.1; xs.2) (ys.1; ys.2) (zs.1; zs.2) pf with
+    | is_app_consV _ x ((_; _) as xs') ((_; _) as ys') ((_; _) as zs') pf' =>
+      P_cons x xs' ys' zs' pf' (is_appV_rect_sig A P P_cons P_nil xs' ys' zs' pf')
+    | is_app_nilV _ ys' =>
+      P_nil ys'
+    end.
+
+  Canonical Structure is_app_rect_lift : Lifted.t :=
+    {| Lifted.base := is_app_rect;
+       Lifted.lift := is_appV_rect_sig |}.
+
+  Fixpoint is_appV_ind_sig
+           (A : Type)
+           (P :
+              forall (xs ys zs : {n:nat & vector A n}),
+                is_appV A xs.& ys.& zs.& -> Prop)
+           (P_cons :
+              forall (x : A) (xs ys zs : {n:nat & vector A n})
+                     (tl : is_appV A xs.& ys.& zs.&),
+                P xs.& ys.& zs.& tl ->
+                P (S xs.1; consV A xs.1 x xs.2)
+                  ys.&
+                  (S zs.1; consV A zs.1 x zs.2)
+                  (is_app_consV A x xs.& ys.& zs.& tl))
+           (P_nil :
+              forall (ys : {n:nat & vector A n}),
+                P (O; nilV A) ys.& ys.& (is_app_nilV A ys))
+           (xs ys zs : {n:nat & vector A n})
+           (pf : is_appV A xs.& ys.& zs.&) {struct pf} :
+    P xs.& ys.& zs.& pf :=
+    match pf as pf in is_appV _ ((_; _) as xs) ((_; _) as ys) ((_; _) as zs) return P (xs.1; xs.2) (ys.1; ys.2) (zs.1; zs.2) pf with
+    | is_app_consV _ x ((_; _) as xs') ((_; _) as ys') ((_; _) as zs') pf' =>
+      P_cons x xs' ys' zs' pf' (is_appV_ind_sig A P P_cons P_nil xs' ys' zs' pf')
+    | is_app_nilV _ ys' =>
+      P_nil ys'
+    end.
+
+  Canonical Structure is_app_ind_lift : Lifted.t :=
+    {| Lifted.base := is_app_ind;
+       Lifted.lift := is_appV_ind_sig |}.
 
   (* Does the lifted type former have the expected type? *)
   Example check_is_appV
@@ -39,17 +98,17 @@ Section Append.
   (* Does the lifted constructor for is_app_cons have the expected type? *)
   Example check_is_app_consV
     : forall (A : Type) (x : A) (xs ys zs : {n:nat & vector A n}),
-      is_appV A (xs.1; xs.2) (ys.1; ys.2) (zs.1; zs.2) ->
+      is_appV A xs.& ys.& zs.& ->
       is_appV A
               (S xs.1; consV A xs.1 x xs.2)
-              (ys.1; ys.2)
+              ys.&
               (S zs.1; consV A zs.1 x zs.2) :=
     is_app_consV.
 
   (* Does the lifted constructor for is_app_nil have the expected type? *)
   Example check_is_app_nilV
     : forall (A : Type) (ys : {n:nat & vector A n}),
-      is_appV A (O; nilV A) (ys.1; ys.2) (ys.1; ys.2) :=
+      is_appV A (O; nilV A) ys.& ys.& :=
     is_app_nilV.
 
   (* Get the size of an is_app proof. *)
@@ -96,8 +155,97 @@ Section Permute.
            (y :: x :: xs)
   | perm_trans (xs ys zs : list A) :
       perm xs ys -> perm ys zs -> perm xs zs.
-
   Lift list vector in @perm as ..V.
+
+  Fixpoint permV_rect_sig
+           (A : Type)
+           (P :
+              forall (xs ys : {n:nat & vector A n}),
+                permV A xs.& ys.& -> Type)
+           (P_nil : P (O; nilV A) (O; nilV A) (perm_nilV A))
+           (P_skip :
+              forall (x : A) (xs ys : {n:nat & vector A n})
+                     (pf' : permV A xs.& ys.&),
+                P xs.& ys.& pf' ->
+                P (S xs.1; consV A xs.1 x xs.2)
+                  (S ys.1; consV A ys.1 x ys.2)
+                  (perm_skipV A x xs.& ys.& pf'))
+           (P_swap :
+              forall (x y : A) (xs : {n:nat & vector A n}),
+                P (S (S xs.1); consV A (S xs.1) x (consV A xs.1 y xs.2))
+                  (S (S xs.1); consV A (S xs.1) y (consV A xs.1 x xs.2))
+                  (perm_swapV A x y xs.&))
+           (P_trans :
+              forall (xs ys zs : {n:nat & vector A n})
+                     (pf' : permV A xs.& ys.&)
+                     (_ : P xs.& ys.& pf')
+                     (pf'' : permV A ys.& zs.&)
+                     (_ : P ys.& zs.& pf''),
+                P xs.& zs.& (perm_transV A xs.& ys.& zs.& pf' pf''))
+           (xs ys : {n:nat & vector A n})
+           (pf : permV A xs.& ys.&) {struct pf} :
+    P xs.& ys.& pf :=
+    match pf as pf in permV _ ((_; _) as xs) ((_; _) as ys) return P (xs.1; xs.2) (ys.1; ys.2) pf with
+    | perm_nilV _ =>
+      P_nil
+    | perm_skipV _ x ((_; _) as xs') ((_; _) as ys') pf' =>
+      P_skip x xs' ys' pf' (permV_rect_sig A P P_nil P_skip P_swap P_trans xs' ys' pf')
+    | perm_swapV _ x y ((_; _) as xs') =>
+      P_swap x y xs'
+    | perm_transV _ ((_; _) as xs') ((_; _) as ys') ((_; _) as zs') pf' pf'' =>
+      P_trans xs' ys' zs'
+              pf' (permV_rect_sig A P P_nil P_skip P_swap P_trans xs' ys' pf')
+              pf'' (permV_rect_sig A P P_nil P_skip P_swap P_trans ys' zs' pf'')
+    end.
+
+  Canonical Structure perm_rect_lift : Lifted.t :=
+    {| Lifted.base := perm_rect;
+       Lifted.lift := permV_rect_sig |}.
+
+  Fixpoint permV_ind_sig
+           (A : Type)
+           (P :
+              forall (xs ys : {n:nat & vector A n}),
+                permV A xs.& ys.& -> Prop)
+           (P_nil : P (O; nilV A) (O; nilV A) (perm_nilV A))
+           (P_skip :
+              forall (x : A) (xs ys : {n:nat & vector A n})
+                     (pf' : permV A xs.& ys.&),
+                P xs.& ys.& pf' ->
+                P (S xs.1; consV A xs.1 x xs.2)
+                  (S ys.1; consV A ys.1 x ys.2)
+                  (perm_skipV A x xs.& ys.& pf'))
+           (P_swap :
+              forall (x y : A) (xs : {n:nat & vector A n}),
+                P (S (S xs.1); consV A (S xs.1) x (consV A xs.1 y xs.2))
+                  (S (S xs.1); consV A (S xs.1) y (consV A xs.1 x xs.2))
+                  (perm_swapV A x y xs.&))
+           (P_trans :
+              forall (xs ys zs : {n:nat & vector A n})
+                     (pf' : permV A xs.& ys.&)
+                     (_ : P xs.& ys.& pf')
+                     (pf'' : permV A ys.& zs.&)
+                     (_ : P ys.& zs.& pf''),
+                P xs.& zs.& (perm_transV A xs.& ys.& zs.& pf' pf''))
+           (xs ys : {n:nat & vector A n})
+           (pf : permV A xs.& ys.&) {struct pf} :
+    P xs.& ys.& pf :=
+    match pf as pf in permV _ ((_; _) as xs) ((_; _) as ys) return P (xs.1; xs.2) (ys.1; ys.2) pf with
+    | perm_nilV _ =>
+      P_nil
+    | perm_skipV _ x ((_; _) as xs') ((_; _) as ys') pf' =>
+      P_skip x xs' ys' pf' (permV_ind_sig A P P_nil P_skip P_swap P_trans xs' ys' pf')
+    | perm_swapV _ x y ((_; _) as xs') =>
+      P_swap x y xs'
+    | perm_transV _ ((_; _) as xs') ((_; _) as ys') ((_; _) as zs') pf' pf'' =>
+      P_trans xs' ys' zs'
+              pf' (permV_ind_sig A P P_nil P_skip P_swap P_trans xs' ys' pf')
+              pf'' (permV_ind_sig A P P_nil P_skip P_swap P_trans ys' zs' pf'')
+    end.
+
+  Canonical Structure perm_ind_lift : Lifted.t :=
+    {| Lifted.base := perm_ind;
+       Lifted.lift := permV_ind_sig |}.
 
   (* Does the lifted type former have the expected type? *)
   Example check_permV
@@ -112,7 +260,7 @@ Section Permute.
   (* Does the lifted constructor for perm_skip have the expected type? *)
   Example check_perm_skipV
     : forall (A : Type) (x : A) (xs ys : {n:nat & vector A n}),
-      permV A xs ys ->
+      permV A xs.& ys.& ->
       permV A
             (S xs.1; consV A xs.1 x xs.2)
             (S ys.1; consV A ys.1 x ys.2) :=
@@ -129,7 +277,7 @@ Section Permute.
   (* Does the lifted constructor for perm_trans have the expected type? *)
   Example check_perm_transV
     : forall (A : Type) (xs ys zs : {n:nat & vector A n}),
-      permV A xs ys -> permV A ys zs -> permV A xs zs :=
+      permV A xs.& ys.& -> permV A ys.& zs.& -> permV A xs.& zs.& :=
     perm_transV.
 
   (* Get the size of a perm proof. *)
