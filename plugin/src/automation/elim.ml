5ddb3a293eda101017f29a469d80ca9b60a81a7a
Nate Yazdani <nyazdani@cs.washington.edu>
2018-09-13 14:23:54 -0700

    Document code with comments

--- plugin/src/automation/elim.ml
+++ plugin/src/automation/elim.ml
@@ -13,10 +13,26 @@ open Constrextern
 
 open Coqterms
 
+(*
+ * Apply Coq's name-freshening policy (i.e., increment subscript if necessary)
+ * and insert freshened name into the set of used identifiers. Also deanonymize
+ * the name, using Coq's standard default name (modulo freshening).
+ *
+ * See [Namegen.next_name_away] and [Namegen.next_ident_away] to learn more about
+ * Coq's freshening policy.
+ *)
 let freshen_name idset name =
   let id = Namegen.next_name_away name idset in
   Name id, Id.Set.add id idset
 
+(*
+ * Freshen all declarations in a rel context, from outermost to innermost
+ * bindings, using Coq's standard freshing policy. In effect, append/increment
+ * numerical subscripts on recycled identifiers, increasing with binding depth.
+ *
+ * See [Namegen.next_name_away] and [Namegen.next_ident_away] to learn more about
+ * Coq's freshening policy.
+ *)
 let freshen_context idset ctxt =
   Rel.fold_outside
     (fun decl (ctxt, idset) ->
@@ -26,26 +42,60 @@ let freshen_context idset ctxt =
     ctxt
     ~init:(Rel.empty, idset)
 
+(*
+ * Build a surface-level pattern (i.e., [Constrexpr.cases_pattern_expr]) from a
+ * rel context with unique identifiers (e.g., freshened beforehand), with nested
+ * pattern-match on each sigma-type argument in order to induce definitional
+ * eta-expansion.
+ *
+ * The head symbol (a [global_reference]) must be either an inductive type or a
+ * constructor. The rel context should be real-argument arity. (Decoding the Coq
+ * parlance, "arity" means the type signature as a rel context, and a "real
+ * argument" is a local assumption; hence, parameters are excluded and local
+ * definitions are folded into the local assumptions' types.)
+ *
+ * The identifier set is used to build an unambiguous reference to the head
+ * symbol and to the sigma type's constructor.
+ *
+ * The number of parameters is used to insert that many "skip patterns"
+ * (i.e., underscores) before the real arguments, as required by Coq.
+ *)
 let eta_extern_pattern idset npar head ctxt =
   let extern_ref = Constrextern.extern_reference idset in
-  let skip_pat = CAst.make (CPatAtom None) in
+  let skip_pat = CAst.make (CPatAtom None) in (* pattern is `_` *)
   let bind_pat =
-    let sigT_pat =
+    let sigT_pat = (* pattern is `@existT _ _ _ _` *)
       let existT_ref = Lazy.force Coqlib.coq_existT_ref |> extern_ref in
       CAst.make (CPatCstr (existT_ref, Some (List.make 4 skip_pat), []))
     in
     fun decl ->
       if applies sigT (rel_type decl) then
+        (* pattern is `(@existT _ _ _ _) as x` *)
         CAst.make (CPatAlias (sigT_pat, CAst.make (rel_name decl)))
       else
+        (* pattern is `x` *)
         CAst.make (CPatAtom (reference_of_name (rel_name decl)))
   in
+  (* pattern is `@head _ ... x|((@existT _ _ _ _) as x) ...` *)
   CAst.make
     (CPatCstr
        (extern_ref head,
         Some (List.rev_map bind_pat ctxt |> List.addn npar skip_pat),
         []))
 
+(*
+ * Externalize a variant of the well-typed term, in which every variable with
+ * sigma type is eta-expanded (e.g., `x` --> `existT (projT1 x) (projT2 x)`),
+ * unless eta-expansion is already available definitionally (e.g.,
+ * `x := existT y z` in context or environment). Moreover, every case analysis
+ * of a form with a field of sigma type is extended to pattern-match each field
+ * of sigma type, therefore inducing a definitional eta-expansion.
+ *
+ * The purpose of this function is to "eta-externalize" an eliminator of a
+ * lifted inductive type and then internalize (type-check) the resulting
+ * expression to serve as the lifting of the corresponding eliminator of the
+ * base type.
+ *)
 let rec eta_extern env evm idset term =
   let raw_extern = EConstr.of_constr %> extern_constr ~lax:true false env evm in
   match kind term with
@@ -68,7 +118,6 @@ let rec eta_extern env evm idset term =
       (eta_extern env evm idset term,
        CastConv (eta_extern env evm idset typ))
   | Prod (name, typ, body) ->
-    (* let ctxt, body = Term.decompose_prod_assum term in *)
     let name, idset' = freshen_name idset name in
     mkProdC
       ([CAst.make name],
@@ -76,7 +125,6 @@ let rec eta_extern env evm idset term =
        eta_extern env evm idset typ,
        eta_extern (push_local (name, typ) env) evm idset' body)
   | Lambda (name, typ, body) ->
-    (* let ctxt, body = Term.decompose_lam_assum term in *)
     let name, idset' = freshen_name idset name in
     mkLambdaC
       ([CAst.make name],
