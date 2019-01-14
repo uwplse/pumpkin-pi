5744e4053fd0540fc800c3179581fd20b3d683a7
Nate Yazdani <nyazdani@cs.washington.edu>
2019-01-13 21:02:52 -0800

    Comment code

--- plugin/src/automation/desugar.ml
+++ plugin/src/automation/desugar.ml
@@ -8,11 +8,21 @@ open Inductiveops
 open CErrors
 open Coqterms
 
+(*
+ * Give a "reasonable" name to each anonymous local declaration in the relative
+ * context. Name generation is according to standard Coq policy (cf., Namegen)
+ * and does not guarantee freshness, but term type-checking is only sensitive to
+ * anonymity. (Names are freshened by subscription when printed.)
+ *)
 let deanonymize_context env evm ctxt =
   List.map EConstr.of_rel_decl ctxt |>
   Namegen.name_context env evm |>
   List.map (EConstr.to_rel_decl evm)
 
+(*
+ * Bind all local declarations in the relative context onto the body term as
+ * products, substituting away (i.e., zeta-reducing) any local definitions.
+ *)
 let smash_prod_assum ctxt body =
   Rel.fold_inside
     (fun body decl ->
@@ -22,6 +32,10 @@ let smash_prod_assum ctxt body =
     ~init:body
     ctxt
 
+(*
+ * Bind all local declarations in the relative context onto the body term as
+ * lambdas, substituting away (i.e., zeta-reducing) any local definitions.
+ *)
 let smash_lam_assum ctxt body =
   Rel.fold_inside
     (fun body decl ->
@@ -31,18 +45,37 @@ let smash_lam_assum ctxt body =
     ~init:body
     ctxt
 
+(*
+ * Zeta-reduce any local definitions occurring in the leading prefix of product
+ * and let bindings on the term. In other words, zeta-reduce all let expressions
+ * above the first non-product, non-let expression.
+ *)
 let contract_prod_assum term =
   let ctxt, body = decompose_prod_assum term in
   smash_prod_assum ctxt body
 
+(*
+ * Zeta-reduce any local definitions occurring in the leading prefix of lambda
+ * and let bindings on the term. In other words, zeta-reduce all let expressions
+ * above the first non-lambda, non-let expression.
+ *)
 let contract_lam_assum term =
   let ctxt, body = decompose_lam_assum term in
   smash_lam_assum ctxt body
 
+(*
+ * Instantiate a local assumption as a local definition, using the provided term
+ * as its definition.
+ *
+ * Raises an assertion error if the local declaration is not a local assumption.
+ *)
 let define_rel_decl body decl =
   assert (is_rel_assum decl);
   rel_defin (rel_name decl, body, rel_type decl)
 
+(*
+ * TODO
+ *)
 let factor_assums rels ctxt =
   let k = List.length rels in
   let len = Rel.length ctxt in
@@ -55,15 +88,25 @@ let factor_assums rels ctxt =
     rels;
   Array.to_list ctxt
 
+(*
+ * Extract the components of an inductive type: the (universe-instantiated)
+ * inductive name, the sequence of parameters, and the sequence of indices.
+ *)
 let decompose_indvect ind_type =
   let pind, args = decompose_appvect ind_type |> on_fst destInd in
   let nparam = inductive_nparams (out_punivs pind) in
   let params, indices = Array.chop nparam args in
   pind, params, indices
 
+(*
+ * Same as decompose_indvect but converts the result arrays into lists.
+ *)
 let decompose_ind ind_type =
   decompose_indvect ind_type |> on_pi2 Array.to_list |> on_pi3 Array.to_list
 
+(*
+ * TODO
+ *)
 let summarize_free_inductive nb ind_type =
   let pind, params, indices = decompose_ind ind_type in
   let ind_fam =
@@ -78,11 +121,24 @@ let summarize_free_inductive nb ind_type =
   in
   ind_fam, ind_rels
 
+(*
+ * Construct a relative context, consisting of only local assumptions,
+ * quantifying over instantiations of the inductive family.
+ *
+ * In other words, the output relative context assumes all indices (in canonical
+ * order) and then a value of the inductive family instantiated with those
+ * indices.
+ *
+ * Note that an inductive family is an inductive name with parameter terms.
+ *)
 let build_inductive_context env ind_fam ind_name =
   let ind_type = build_dependent_inductive env ind_fam in
   let ind_decl = rel_assum (ind_name, ind_type) in
   get_arity env ind_fam |> fst |> Rel.add ind_decl |> Termops.smash_rel_context
 
+(*
+ * TODO
+ *)
 let wrap_fixpoint fun_len ind_rels fun_ctxt =
   let fix_head = mkRel (fun_len + 1) in
   let fix_args =
@@ -95,6 +151,9 @@ let wrap_fixpoint fun_len ind_rels fun_ctxt =
   in
   recompose_lam_assum fun_ctxt (mkApp (fix_head, fix_args))
 
+(*
+ * TODO
+ *)
 let order_fixpoint ind_ctxt ind_rels fun_ctxt fun_type fun_term =
   (* TODO: Probably cleaner to factor+smash in one go *)
   let fun_ctxt = factor_assums ind_rels fun_ctxt in
@@ -103,6 +162,9 @@ let order_fixpoint ind_ctxt ind_rels fun_ctxt fun_type fun_term =
   let fun_term = fun_term |> lift |> smash_lam_assum fun_ctxt in
   Util.map_pair (recompose_lam_assum ind_ctxt) (fun_type, fun_term)
 
+(*
+ * TODO
+ *)
 let premise_of_case ind_fam rec_name motive (ctxt, body) =
   let nb = Rel.length ctxt in
   let ind_head = dest_ind_family ind_fam |> on_fst mkIndU |> applist in
@@ -122,6 +184,9 @@ let premise_of_case ind_fam rec_name motive (ctxt, body) =
   in
   List.fold_left_i fixpoint_to_recurrence 1 body ctxt
 
+(*
+ * TODO
+ *)
 let split_functional env fun_term cons_sum =
   let cons = build_dependent_constructor cons_sum in
   let env = Environ.push_rel_context cons_sum.cs_args env in
@@ -132,6 +197,9 @@ let split_functional env fun_term cons_sum =
   in
   deanonymize_context env Evd.empty cons_sum.cs_args, body
 
+(*
+ * TODO
+ *)
 let motive_of_predicate env ind_fam pred =
   let ind = dest_ind_family ind_fam |> fst |> out_punivs in
   let ind_sort = get_arity env ind_fam |> snd in
@@ -143,7 +211,9 @@ let motive_of_predicate env ind_fam pred =
   else
     pred
 
-(* Assumes that the fixed-point has already been regularized *)
+(*
+ * TODO
+ *)
 let eliminate_fixpoint env evm ind_fam fix_name fun_type fun_term =
   let fix_decl = rel_assum (fix_name, Vars.lift (-1) fun_type) in
   let env = Environ.push_rel fix_decl env in
@@ -158,7 +228,9 @@ let eliminate_fixpoint env evm ind_fam fix_name fun_type fun_term =
   let premises = Array.cons motive minors in
   mkApp (elim, Array.append params premises) |> Vars.lift (-1)
 
-(* Convenient wrapper around eliminate_fixpoint *)
+(*
+ * TODO
+ *)
 let eliminate_match env evm info pred discr cases =
   let env = Environ.push_rel (rel_assum (Name.Anonymous, pred)) env in
   let pred, discr, cases =
@@ -177,7 +249,9 @@ let eliminate_match env evm info pred discr cases =
   mkApp (elim, Array.concat [params; premises; indices; [|discr|]]) |>
   Vars.lift (-1)
 
-(* Translate each match expression into an equivalent eliminator application *)
+(*
+ * TODO
+ *)
 let desugar_fix_match env evm term =
   let evm = ref evm in
   let rec aux env term =
