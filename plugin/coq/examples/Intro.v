21eea4fdb6a98ef0c4afb63bd1b2cb9908235bbb
Talia Ringer <tringer@cs.washington.edu>
2019-03-18 11:25:34 -0700

    intro example, and move paper examples

---
+++ plugin/coq/examples/Intro.v
@@ -0,0 +1,58 @@
+(*
+ * Example from Section 1, using DEVOID
+ *)
+
+Require Import Vector.
+Require Import List.
+
+Require Import Ornamental.Ornaments.
+
+(* 
+ * Coq's vector definition is syntactically different from the version defined 
+ * in the paper, which is simplified for the sake of presentation.
+ * For simplicity, we define syntax for this.
+ *)
+Definition vector := Vector.t.
+
+(*
+ * map_length from the list standard library
+ *)
+Check map_length.
+
+(*
+ * Coq's vector map.
+ *)
+Check Vector.map.
+
+(* --- Bonus material --- *)
+
+(*
+ * We can get Vector.map from List.map.
+ *)
+
+Desugar List.map as list_map'.
+Find ornament list vector as ltv.
+Lift list vector in list_map' as map_p.
+(* TODO use unpack *)
+Definition map_u {T1} {T2} (f : T1 -> T2) {n : nat} (v : vector T1 n) :=
+  projT2 (map_p T1 T2 f (existT _ n v)). 
+
+(* User-friendly version *)
+Program Definition map {T1} {T2} (f : T1 -> T2) {n : nat} (v : vector T1 n) : vector T2 n :=
+  map_u f v.
+Next Obligation.
+  induction v.
+  - auto.
+  - simpl. f_equal. auto. (* TODO why don't we need the methodology here? *)
+Defined.
+
+(* We can show it's the same as Coq's map *)
+Lemma map_correct :
+  forall {T1} {T2} (f : T1 -> T2) {n : nat} (v : vector T1 n),
+    map f v = Vector.map f v.
+Proof.
+  intros. induction v.
+  - auto.
+  - simpl. rewrite <- IHv. unfold map. simpl.
+    destruct (map_obligation_1 T1 T2 f n v). auto. 
+Qed.
