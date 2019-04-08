060a67de429a9d7c556b53a9cbe4be690c02bf0c
Talia Ringer <tringer@cs.washington.edu>
2019-04-08 15:29:16 -0700

    get most of the args

--- plugin/src/frontend.ml
+++ plugin/src/frontend.ml
@@ -112,8 +112,19 @@ let prove_section env evd orn =
           debug_env env_lemma "env_lemma";
           let (_, body) =
             List.fold_right
+              (* @eq_ind_r (list T) l2
+  (fun l3 : list T =>
+   @eq (list T) (@Datatypes.cons T t l3)
+     (@Datatypes.cons T t l2))
+  (@eq_refl (list T) (@Datatypes.cons T t l2)) l1
+  H*)
+              
               (fun _ (h_eq, b) ->
-                (shift (shift h_eq), mkAppl (eq_ind, [(*TODO;*) shift (shift b); h_eq])))
+                let h_eq_rel = destRel h_eq in
+                let (_, _, h_eq_typ) = CRD.to_tuple @@ lookup_rel h_eq_rel env_lemma in
+                let typ :: rec1 :: rec2 :: _ = unfold_args h_eq_typ in
+                (* TOOD after rec1, the eq function *)
+                (shift (shift h_eq), mkAppl (eq_ind, [shift typ; shift rec1; shift (shift b); shift rec2; h_eq])))
               recs
               (mkRel 1, refl)
           in reconstruct_lambda env_lemma body)
