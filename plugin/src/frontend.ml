0d10741f994826cb6b8ce8a37dd257c9b5c48294
Talia Ringer <tringer@cs.washington.edu>
2019-04-11 12:40:31 -0700

    move other eq to use new eq type, too

--- plugin/src/frontend.ml
+++ plugin/src/frontend.ml
@@ -102,19 +102,22 @@ let section_eq_lemmas env evd a_typ =
             (push_local (Anonymous, r_eq) e_r, off + 2))
           recs
           (env_c_b, 0)
-      in debug_env env_lemma "env_lemma";
+      in
       let (body, _, _) =
         List.fold_right
           (fun _ (b, h_eq, inner) ->
-            let h_eq_rel = destRel h_eq in
-            let (_, _, h_eq_typ) = CRD.to_tuple @@ lookup_rel h_eq_rel env_lemma in
-            (* TODO ASAP move eq for both appl and this *)
-            let typ :: rec1 :: rec2 :: _ = unfold_args h_eq_typ in
+            let h_eq_r = destRel h_eq in
+            let (_, _, h_eq_t) = CRD.to_tuple @@ lookup_rel h_eq_r env_lemma in
+            debug_term env_lemma h_eq_t "h_eq_t";
+            let app = dest_eq h_eq_t in
+            let typ = app.at_type in
+            let r1 = app.trm1 in
+            let r2 = app.trm2 in
             let inner = shift_by 2 inner in (* nested equality proof *)
-            let abs_inner = all_eq_substs (shift (shift rec1), mkRel 1) (shift inner) in
-            let new_inner = all_eq_substs (shift (shift rec1), shift (shift rec2)) (shift inner) in
+            let abs_inner = all_eq_substs (shift (shift r1), mkRel 1) (shift inner) in
+            let new_inner = all_eq_substs (shift (shift r1), shift (shift r2)) (shift inner) in
             let eq_ind_rel = mkLambda (Anonymous, shift typ, mkAppl (eq, [shift (shift typ); shift inner; abs_inner])) in
-            (mkAppl (eq_ind, [shift typ; shift rec1; eq_ind_rel; shift (shift b); shift rec2; h_eq]), shift (shift h_eq), new_inner))
+            (mkAppl (eq_ind, [shift typ; shift r1; eq_ind_rel; shift (shift b); shift r2; h_eq]), shift (shift h_eq), new_inner))
           recs
           (refl, mkRel 1, c_body)
       in reconstruct_lambda env_lemma body)
