1caa158589c5c0f268772e2c0940074133e3f286
Talia Ringer <tringer@cs.washington.edu>
2019-04-18 14:37:26 -0700

    use retraction proof (modulo eta)

--- plugin/coq/examples/Search.v
+++ plugin/coq/examples/Search.v
@@ -87,51 +87,11 @@ Proof.
   exact ltv_section.
 Qed.
 
-(* --- not yet automated --- *)
-
-(*
- * To prove section and retraction, we need to show that equalities are
- * preserved in the inductive cases. For list this is simple:
- *)
-Lemma eq_cons:
-  forall {T : Type} (t : T) (l1 : list T) (l2 : list T),
-    l1 = l2 ->
-    cons t l1 = cons t l2.
-Proof.
-  intros. rewrite <- H. auto.
-Qed.
-
-(*
- * Luckily, we have a tool that can give us the version over vectors :)
- *)
-Lift list vector in @eq_cons as eq_sigT_cons_p.
-Definition eq_sigT_cons {T} t n1 v1 n2 v2 := (* partial unpack *)
-  eq_sigT_cons_p T t (existT _ n1 v1) (existT _ n2 v2).
-
-Theorem retraction:
+Theorem retraction: (* TODO build eta into the actual algorithm*)
   forall {T : Type} (v : sigT (fun n => vector T n)),
     promote (forget v) = v.
 Proof.
-  intros. symmetry. induction v; induction p.
-  - reflexivity.
-  - apply eq_sigT_cons. apply IHp.
-Qed.  
+  intros. pose proof (ltv_retraction T v). induction v. apply H.
+Qed.
 
-Print retraction.
-
-(*
-
-fun (h : T) (n : nat) (p0 : VectorDef.t T n) (IHp : @eq (@sigT nat (fun H : nat => t T H)) (@existT nat (fun n0 : nat => t T n0) n p0)
-                    (ltv T (ltv_inv T (@existT nat (fun n0 : nat => t T n0) n p0)))) =>
-  eq_ind
-    (sigT (vector T)) 
-    (existT _ n p0) 
-    (fun (l3 : sigT (vector T)) => 
-       existT _ (S n) (cons T h n p0) =
-       existT _ (S (projT1 l3)) (cons T h (projT1 l3) (projT2 l3)))
-    (eq_refl (existT _ (S n) (cons T h n p0))) 
-    (existT _ 
-      (Coq.Init.Specif.projT1 nat (λ (_ : nat) . (t (A [Rel 7]) (_ [Rel 1]))) (Search.ltv (A [Rel 6]) (Search.ltv_inv (A [Rel 6]) (existT nat (λ (_ : nat) . (t (A [Rel 7]) (_ [Rel 1]))) (n [Rel 3]) (t [Rel 2]))))) 
-      (Search.ltv (A [Rel 6]) (Search.ltv_inv (A [Rel 6]) (existT nat (λ (_ : nat) . (t (A [Rel 7]) (_ [Rel 1]))) (n [Rel 3]) (t [Rel 2]))))) (_ [Rel 1]))))))
-
- *)
+Print retraction.  
