c93617e32fd1b74db9ea3d13a6a94c43d18931e2
Talia Ringer <tringer@cs.washington.edu>
2019-04-26 09:09:11 -0700

    Improve automatically generated coherence type by using constants

--- plugin/src/automation/coherence.ml
+++ plugin/src/automation/coherence.ml
@@ -12,7 +12,8 @@ open Lifting
  * Return the coherence proof term and its type
  *)
 let prove_coherence env evd orn =
-  let env_coh = zoom_env zoom_lambda_term env orn.promote in
+  let promote = lookup_definition env orn.promote in
+  let env_coh = zoom_env zoom_lambda_term env promote in
   let a = mkRel 1 in
   let is = on_type unfold_args env_coh evd a in
   let b_sig = mkAppl (orn.promote, snoc a is) in
@@ -21,7 +22,7 @@ let prove_coherence env evd orn =
   let ib_typ = b_sig_typ.index_type in
   let proj_ib = project_index b_sig_typ b_sig in
   let refl = apply_eq_refl { typ = ib_typ; trm = proj_ib } in
-  let refl_typ = apply_eq { at_type = ib_typ; trm1 = ib; trm2 = proj_ib } in
+  let refl_typ = apply_eq { at_type = ib_typ; trm1 = proj_ib; trm2 = ib } in
   let coh = reconstruct_lambda env_coh refl in
   let coh_typ = reconstruct_product env_coh refl_typ in
   (coh, coh_typ)
