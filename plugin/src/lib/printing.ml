9325404c25232d08148f06a708f68ce78039a4d8
Talia Ringer <tringer@cs.washington.edu>
2019-06-13 18:32:52 -0400

    fix bug in printing

--- plugin/src/lib/printing.ml
+++ plugin/src/lib/printing.ml
@@ -84,7 +84,7 @@ let rec term_as_string (env : env) (trm : types) =
      Printf.sprintf
        "(let (%s : %s) := %s in %s)"
        (name_as_string n)
-       (term_as_string env typ)
+       (term_as_string env trm)
        (term_as_string env typ)
        (term_as_string (push_let_in (n, e, typ) env) e)
   | App (f, xs) ->
