0e1928383e1a39f032c22dad588832d01a001471
Talia Ringer <tringer@cs.washington.edu>
2019-06-18 12:31:52 -0700

    another example

--- plugin/coq/examples/Projections.v
+++ plugin/coq/examples/Projections.v
@@ -11,9 +11,19 @@ Theorem convert:
     v = existT _ (projT1 v) (projT2 v).
 Proof.
   Fail reflexivity.
-Admitted.
+Abort.
 
 (*
  * So in general, if you want to preserve definitional equalities,
  * you need to expand every v to existT _ (projT1 v) (projT2 v).
  *)
+
+(*
+ * We can optimize some terms, though, and avoid packing in some cases, since:
+ *)
+Theorem convert:
+  forall (T : Type) (n : nat) (v : vector T n),
+    existT _ v n = existT _ (projT1 (existT _ v n)) (projT2 (existT _ v n)).
+Proof.
+  reflexivity.
+Qed.
