7b44b9b193937d437aa1e69ff7225ccb4cbf8e0c
Talia Ringer <tringer@cs.washington.edu>
2019-08-12 12:09:48 -0700

    evar_map in abtraction

--- plugin/src/components/abstraction.ml
+++ plugin/src/components/abstraction.ml
@@ -16,25 +16,21 @@ let abstract_arg env sigma i typ =
   let arg = get_arg i typ in
   let sigma, arg_typ = reduce_type env sigma arg in
   let args = reindex i (mkRel 1) (shift_all (unfold_args typ)) in
-  mkLambda (Anonymous, arg_typ, mkAppl (first_fun typ, args))
+  sigma, mkLambda (Anonymous, arg_typ, mkAppl (first_fun typ, args))
 
 (* Replace all occurrences of the first term in the second term with Rel 1,
  * lifting de Bruijn indices as needed. The notion of term equality is modulo
  * alpha, casts, application grouping, and universes.
 *)
 let abstract_subterm sub term =
-  (* Allocate a binding slot for the abstracted subterm *)
-  let sub = Vars.lift 1 sub in
-  let term = Vars.lift 1 term in
   let rec surgery (nb, sub) term =
     match eq_constr_head sub term with
     | Some args ->
       mkApp (mkRel (nb + 1), args)
     | None ->
       Constr.map_with_binders
-        (fun (nb, sub) -> nb + 1, Vars.lift 1 sub)
+        (fun (nb, sub) -> nb + 1, shift sub)
         surgery
         (nb, sub)
         term
-  in
-  surgery (0, sub) term
+  in surgery (0, shift sub) (shift term)
