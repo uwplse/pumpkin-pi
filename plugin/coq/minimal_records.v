552c71bb5f7913e584537d483f13d8f5637ceee1
Talia Ringer <tringer@cs.washington.edu>
2019-11-27 18:41:19 -0800

    a bunch of tests

--- plugin/coq/minimal_records.v
+++ plugin/coq/minimal_records.v
@@ -122,6 +122,23 @@ Scheme Induction for handwritten_input_4 Sort Type.
 
 Find ornament handwritten_input_4 generated_input_4.
 
+Record handwritten_input_5 := MkInput5
+{
+  field1'  : bool;
+  field2'    : nat;
+  field3' : bool;
+  field4' : nat;
+  field5' : bool; 
+}.
+
+Definition generated_input_5 := (prod bool (prod nat (prod bool (prod nat bool)))).
+
+Scheme Induction for handwritten_input_5 Sort Set.
+Scheme Induction for handwritten_input_5 Sort Prop.
+Scheme Induction for handwritten_input_5 Sort Type.
+
+Find ornament handwritten_input_5 generated_input_5.
+
 Definition generated_input_param_test (T1 T2 T3 : Type) := (prod T1 (prod T2 T3)).
 
 Record handwritten_input_param_test (T1 T2 T3 : Type) := MkInputT
@@ -143,6 +160,45 @@ Find ornament handwritten_input_param_test generated_input_param_test. (* TODO c
 (* TODO test: failure cases, dependent parameters, eta expanded or not expanded variations, 2 fields, 4 fields, taking prod directly, etc *)
 (* TODO check test results *)
 (* TODO integrate into below *)
+
+Definition generated_input_param_test2 (T1 T2 T3 T4 : Type) := (prod T1 (prod T2 (prod T3 T4))).
+
+Record handwritten_input_param_test2 (T1 T2 T3 T4 : Type) := MkInputT2
+{
+  firstT' : T1;
+  secondT' : T2;
+  thirdT' : T3;
+  fourthT' : T4;
+}.
+
+Scheme Induction for handwritten_input_param_test2 Sort Set.
+Scheme Induction for handwritten_input_param_test2 Sort Prop.
+Scheme Induction for handwritten_input_param_test2 Sort Type.
+
+(* The most basic test: When this works, should just give us fst *)
+(* TODO set options to prove equiv: Set DEVOID search prove equivalence. Then get working. Then try w/ params. Then clean. Then do lift, same process.*)
+Find ornament handwritten_input_param_test2 generated_input_param_test2. (* TODO can omit once lift works *)
+(*Fail Lift handwritten_input generated_input in firstBool as lifted_firstBool.*)
+
+Record handwritten_input_param_test3 (T : Type) (t : T) (F : T -> Prop) := mkInput3
+{
+  firstT'' : F t;
+  secondT'' : T;
+  thirdT'' : exists t', t <> t' -> F t';
+}.
+
+Definition generated_input_param_test3 (T : Type) (t : T) (F : T -> Prop) :=
+  (prod (F t) (prod T (exists t', t <> t' -> F t'))).
+
+
+Scheme Induction for handwritten_input_param_test3 Sort Set.
+Scheme Induction for handwritten_input_param_test3 Sort Prop.
+Scheme Induction for handwritten_input_param_test3 Sort Type.
+
+(* The most basic test: When this works, should just give us fst *)
+(* TODO set options to prove equiv: Set DEVOID search prove equivalence. Then get working. Then try w/ params. Then clean. Then do lift, same process.*)
+Find ornament handwritten_input_param_test3 generated_input_param_test3. (* TODO can omit once lift works *)
+(*Fail Lift handwritten_input generated_input in firstBool as lifted_firstBool.*)
 
 Lemma firstBool_spec (g : generated_input) :
   generated_firstBool g = firstBool (g2h_input g).
