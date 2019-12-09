9c64583953e22864f99a0d91e4bffe67fa555488
Talia Ringer <tringer@cs.washington.edu>
2019-12-09 14:09:37 -0800

    for prettier results, also treat andb as opaque

--- plugin/coq/minimal_records.v
+++ plugin/coq/minimal_records.v
@@ -103,7 +103,7 @@ Lift Generated'.output Handwritten'.output in Generated'.andBools as andBools.
 (*
  * Now lifting to op uses the cached results:
  *)
-Lift Generated'.input Handwritten'.input in Generated'.op as op_1.
+Lift Generated'.input Handwritten'.input in Generated'.op as op_1 { opaque Generated'.Coq_Init_Datatypes_andb }.
 Lift Generated'.output Handwritten'.output in op_1 as op {opaque firstBool numberI secondBool}.
 (*
  * Note that to get prettier results here, we told to treat certain constants as opaque.
