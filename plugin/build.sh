3bfe6f2c3b9b6e699d2313e287e045d21f3acbc9
Talia Ringer <tringer@cs.washington.edu>
2019-12-11 14:38:31 -0800

    broken if you try to clean first...

--- plugin/build.sh
+++ plugin/build.sh
@@ -6,5 +6,6 @@ cd deps/fix-to-elim/plugin
 ./build.sh
 cd ../../..
 echo "building DEVOID"
-coq_makefile -f _CoqProject -o Makefile
-make clean && make && make install
+																																																																																																																																																																																																																																																																																																																																																																																																																																														coq_makefile -f _CoqProject -o Makefile
+make && make install
+
