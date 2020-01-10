c460f88ff6aca5a97712d5dcb6cea877535d5af1
Talia Ringer <tringer@cs.washington.edu>
2020-01-10 12:29:15 -0800

    Test output of smartcache

--- plugin/coq/SmartCache.v
+++ plugin/coq/SmartCache.v
@@ -9,10 +9,17 @@ Set DEVOID lift type. (* <-- Prettier types than the ones Coq infers *)
 Set Nonrecursive Elimination Schemes. (* <--- Preprocess needs induction principles for records *)
 Set DEVOID smart cache.
 
+Module leb.
+
 Lemma leb_implb : forall b1 b2, leb b1 b2 -> implb b1 b2 = true.
 Proof.
   apply leb_implb.
 Qed.
+
+End leb.
+
+Preprocess Module leb as leb' { opaque Bool.leb_implb }.
+Definition leb_implb := leb'.leb_implb.
 
 Definition f (b1 b2 b3 b4 : bool) (H : leb true false) (H0 : leb_implb true false H = leb_implb true false H) := ifb (eqb (negb (andb b1 (orb b2 (xorb b3 b4)))) true) false b4.
 Definition g (b1 b2 b3 b4 : bool) (H : leb true false) (H0 : leb_implb true false H = leb_implb true false H)  := ifb (eqb (negb (orb b1 (andb b2 (xorb b3 b4)))) true) false b4.
@@ -23,3 +30,27 @@ Time Lift Generated'.output Handwritten'.output in f as f'.
 Time Lift Generated'.output Handwritten'.output in g as g'.
 Time Lift Generated'.output Handwritten'.output in h as h'.
 Time Lift Generated'.output Handwritten'.output in i as i'.
+
+Lemma test_f :
+  f = f'.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma test_g :
+  g = g'.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma test_h :
+  h = h'.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma test_i :
+  i = i'.
+Proof.
+  reflexivity.
+Qed.
