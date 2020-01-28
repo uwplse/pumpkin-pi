bd8a51ba91369c5af915e348d5195a8a7262f7a9
Nate Yazdani <nyazdani@cs.washington.edu>
2020-01-27 22:20:15 -0500

    Include testcase lifting eliminator from list to vector

--- plugin/coq/examples/ListToVect.v
+++ plugin/coq/examples/ListToVect.v
@@ -7,7 +7,14 @@ Require Import List.
 
 Require Import Ornamental.Ornaments.
 
+Notation "( x ; y )" := (existT _ x y) (no associativity).
+Notation "p .1" := (projT1 p) (left associativity, at level 8, format "p .1").
+Notation "p .2" := (projT2 p) (left associativity, at level 8, format "p .2").
+Notation "p .&" := (p.1; p.2) (left associativity, at level 6, format "p .&").
+
 Notation vector := Vector.t.
+Notation vnil := Vector.nil.
+Notation vcons := Vector.cons.
 
 (* --- Preprocess --- *)
 
@@ -28,6 +35,19 @@ Preprocess Module List as List' { opaque (* ignore these: *)
 (* --- Search & Lift --- *)
 
 (* We use automatic search here rather than calling Find Ornament ourselves. *)
+
+Definition list_elim A P : P nil -> (forall x xs, P xs -> P (cons x xs)) -> forall xs, P xs :=
+  fun H__nil H__cons xs => @list_rect A P H__nil H__cons xs.
+
+Lift list vector in list_elim as vect_elim.
+
+Check (vect_elim :
+         forall (A : Type) (P : {n : nat & vector A n} -> Type),
+           P (0; vnil A) ->
+           (forall (x : A) (xs : {n : nat & vector A n}),
+               P xs.& -> P (S xs.1; vcons A x xs.1 xs.2)) ->
+           forall (xs : {n : nat & vector A n}),
+             P xs.&).
 
 Lift Module list vector in List' as Vector'.
 
