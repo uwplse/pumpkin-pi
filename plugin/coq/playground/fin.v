65902cd73251f35ee2dd263d70ba922aebd34a33
Talia Ringer <tringer@cs.washington.edu>
2020-10-14 15:15:38 -0700

    prove dep_elim with James proof

--- plugin/coq/playground/fin.v
+++ plugin/coq/playground/fin.v
@@ -1,5 +1,7 @@
 (*
  * Vectors and Fin (also from Anders)
+ * Thanks to James Wilcox for the missing gaps that I needed!
+ * https://gist.github.com/wilcoxjay/10cc817d20ad7148899c3725a1ebf06e
  *)
 Require Import Ornamental.Ornaments.
 Set DEVOID lift type.
@@ -33,14 +35,19 @@ Proof.
   unfold B. intros f. apply Fin.case0. apply f.
 Defined.
 
-(* cons---not sure what this looks like *)
-Parameter dep_constr_B_1 : forall (t : T) (n : nat) (f : B n), B (S n).
+Definition dep_constr_B_1 (t : T) (n : nat) (b : B n) : B (S n) :=
+  fun f =>
+  match f with
+  | Fin.F1 => fun _ => t
+  | Fin.FS f' => fun b' => b' f'
+  end b.
 
 Definition eta_A (n : nat) (a : A n) : A n := a.
 Definition eta_B (n : nat) (b : B n) : B n := b.
 
 (* might need this instead, unsure: *)
 (*Definition eta_A (n m : nat) (H : n = m) (a : A n) : A m := eq_rect m A a n H.*)
+(* and eta B might need to use the tail/hd properties. we'll see *)
 
 Definition dep_elim_A (P : forall n : nat, A n -> Type) (f0 : P 0 dep_constr_A_0)
   (f1 : forall (t : T) (n : nat) (v : A n), P n v -> P (S n) (dep_constr_A_1 t n v)) 
@@ -49,30 +56,26 @@ Definition dep_elim_A (P : forall n : nat, A n -> Type) (f0 : P 0 dep_constr_A_0
 :=
   Vector.t_rect T P f0 f1 n v.
 
-(* TODO stuck on defining these, if possible*)
-Parameter hd : forall (n : nat) (f : B (S n)), T.
-Parameter tail : forall (n : nat) (f : B (S n)), B n.
+Definition hd n (b : B (S n)) : T :=
+  b Fin.F1.
 
-Axiom tail_OK:
+Definition tl n (b : B (S n)) : B n :=
+  fun f => b (Fin.FS f).
+
+Lemma tl_OK:
   forall (n : nat) (b : B (S n)),
-    dep_constr_B_1 (hd n b) n (tail n b) = b.
-
-(* Over vectors, sanity check: *)
-Program Definition hd_vect : forall (n : nat) (f : A (S n)), T.
+    dep_constr_B_1 (hd n b) n (tl n b) = b.
 Proof.
-  intros. apply (Vector.hd f).
-Defined.
-
-Program Definition tl_vect : forall (n : nat) (f : A (S n)), A n.
-Proof.
-  intros. apply (Vector.tl f).
-Defined.
-
-Lemma tail_OK_vect:
-  forall (n : nat) (a : A (S n)),
-    dep_constr_A_1 (hd_vect n a) n (tl_vect n a) = a.
-Proof.
-  intros. symmetry. apply VectorSpec.eta.
+  intros n b.
+  apply funext.
+  intros f.
+  revert b.
+  refine (
+    match f with
+    | Fin.F1 => _
+    | Fin.FS _ => _
+    end
+  ); reflexivity.
 Defined.
 
 Program Definition dep_elim_B (P : forall n : nat, B n -> Type) (f0 : P 0 dep_constr_B_0)
@@ -83,19 +86,9 @@ Proof.
   induction n.
   - rewrite (funext b (fun f : Fin.t 0 => Fin.case0 (fun _ : Fin.t 0 => T) f)); auto.
     intros f. apply Fin.case0. apply f.
-  - pose (f1 (hd n b) n (tail n b) (IHn (tail n b))).
-    replace b with (dep_constr_B_1 (hd n b) n (tail n b)); auto.
-    apply tail_OK.
+  - replace b with (dep_constr_B_1 (hd n b) n (tl n b)); auto.
+    apply tl_OK.
 Defined.
 
-(* FinVec→Vec : {n : ℕ} → FinVec A n → Vec A n
-FinVec→Vec {n = zero}  xs = []
-FinVec→Vec {n = suc _} xs = xs zero ∷ FinVec→Vec (λ x → xs (suc x))
 
-Vec→FinVec : {n : ℕ} → Vec A n → FinVec A n
-Vec→FinVec (xs : Vec A n) (f : Fin n)  = lookup f xs
-
-lookup : ∀ {n} {A : Type ℓ} → Fin n → Vec A n → A
-lookup zero    (x ∷ xs) = x
-lookup (suc i) (x ∷ xs) = lookup i xs*)
 
