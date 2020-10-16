a3de8d7482247a64a9decf18daa7c897a509c863
Talia Ringer <tringer@cs.washington.edu>
2020-10-15 18:53:02 -0700

    functions over fin

--- plugin/coq/playground/fin.v
+++ plugin/coq/playground/fin.v
@@ -4,7 +4,6 @@
  * https://gist.github.com/wilcoxjay/10cc817d20ad7148899c3725a1ebf06e
  *)
 Require Import Ornamental.Ornaments.
-Set DEVOID lift type.
 
 Require Import Coq.Vectors.Fin.
 Require Import Coq.Vectors.Vector.
@@ -12,59 +11,72 @@ Require Import Coq.Vectors.Vector.
 (* needed for this equivalence *)
 Require Import Coq.Logic.FunctionalExtensionality.
 
-Section Config.
+(*
+ * First we need to preprocess to use eliminators, so we can repair later:
+ *)
+Preprocess Module VectorDef as VectorDef' { opaque
+  (* no need: *)
+  Coq.Init.Datatypes Coq.Init.Logic Coq.Init.Peano Coq.Init.Nat
+  Coq.Classes.RelationClasses Coq.Relations.Relation_Definitions Coq.Classes.Morphisms
+  Coq.Arith.PeanoNat Coq.Classes.Morphisms_Prop Coq.Program.Basics 
+  (* needs n-induction: *)
+  VectorDef.rectS VectorDef.caseS' VectorDef.rect2
+  VectorDef.last VectorDef.nth VectorDef.nth_order VectorDef.replace VectorDef.replace_order
+  VectorDef.shiftout VectorDef.shiftrepeat VectorDef.trunc VectorDef.rev_append
+  VectorDef.rev VectorDef.map2 VectorDef.fold_right2 VectorDef.fold_left2
+}.
 
-Parameter T : Type.
+(*
+ * PUMPKIN Pi still gets annoyed about implicit parameters sometimes, so for now we make
+ * T and n explicit.
+ *)
+Definition A (T : Type) (n : nat) := VectorDef'.t T n.
 
-Definition A (n : nat) :=
-  Vector.t T n.
+Definition B (T : Type) (n : nat) := Fin.t n -> T.
 
-Definition B (n : nat) :=
-  Fin.t n -> T.
+(* --- Configuration --- *)
 
-Definition dep_constr_A_0 : A 0 :=
-  Vector.nil T.
+Definition dep_constr_A_0 :=
+  VectorDef'.nil.
 
-Definition dep_constr_A_1 (t : T) (n : nat) (v : A n) : A (S n) :=
-  Vector.cons T t n v.
+Definition dep_constr_A_1 (T : Type) (t : T) (n : nat) (a : A T n) : A T (S n) :=
+  VectorDef'.cons T t n a.
 
-Program Definition dep_constr_B_0 : B 0.
+Program Definition dep_constr_B_0 (T : Type) : B T 0.
 Proof.
   unfold B. intros f. apply Fin.case0. apply f.
 Defined.
 
-Definition dep_constr_B_1 (t : T) (n : nat) (b : B n) : B (S n) :=
+Definition dep_constr_B_1 (T : Type) (t : T) (n : nat) (b : B T n) : B T (S n) :=
   fun f =>
   match f with
   | Fin.F1 => fun _ => t
   | Fin.FS f' => fun b' => b' f'
   end b.
 
-Definition eta_A (n : nat) (a : A n) : A n := a.
-Definition eta_B (n : nat) (b : B n) : B n := b.
+Definition eta_A (T : Type) (n : nat) (a : A T n) : A T n := a.
+Definition eta_B (T : Type) (n : nat) (b : B T n) : B T n := b.
 
 (* TODO for practicality, might need this instead, unsure: *)
 (*Definition eta_A (n m : nat) (H : n = m) (a : A n) : A m := eq_rect m A a n H.*)
 (* and eta B might need to use the tail/hd properties. we'll see *)
 
-Definition dep_elim_A (P : forall n : nat, A n -> Type) (f0 : P 0 dep_constr_A_0)
-  (f1 : forall (t : T) (n : nat) (v : A n), P n v -> P (S n) (dep_constr_A_1 t n v)) 
-  (n : nat) (v : A n) 
-: P n v 
-:=
-  Vector.t_rect T P f0 f1 n v.
+Definition dep_elim_A : forall (T : Type) (P : forall n : nat, A T n -> Type) (f0 : P 0 (dep_constr_A_0 T))
+  (f1 : forall (t : T) (n : nat) (v : A T n), P n v -> P (S n) (dep_constr_A_1 T t n v)) 
+  (n : nat) (v : A T n), P n v 
+:=  VectorDef'.t_rect.
 
-Definition hd n (b : B (S n)) : T :=
+Definition hd (T : Type) n (b : B T (S n)) : T :=
   b Fin.F1.
 
-Definition tl n (b : B (S n)) : B n :=
+Definition tl (T : Type) n (b : B T (S n)) : B T n :=
   fun f => b (Fin.FS f).
 
 Lemma eta_dep_constr_B_0:
-  forall (b : B 0),
-    dep_constr_B_0 = b.
+  forall (T : Type) (b : B T 0),
+    dep_constr_B_0 T = b.
 Proof.
-  intros b.
+  intros T b.
   apply functional_extensionality_dep_good.
   intros f.
   apply Fin.case0.
@@ -72,10 +84,10 @@ Proof.
 Defined.
 
 Lemma eta_dep_constr_B_1:
-  forall (n : nat) (b : B (S n)),
-    dep_constr_B_1 (hd n b) n (tl n b) = b.
+  forall (T : Type) (n : nat) (b : B T (S n)),
+    dep_constr_B_1 T (hd T n b) n (tl T n b) = b.
 Proof.
-  intros n b.
+  intros T n b.
   apply functional_extensionality_dep_good.
   intros f.
   revert b.
@@ -87,37 +99,37 @@ Proof.
   ); reflexivity.
 Defined.
 
-Program Definition dep_elim_B (P : forall n : nat, B n -> Type) (f0 : P 0 dep_constr_B_0)
-  (f1 : forall (t : T) (n : nat) (f : B n), P n f -> P (S n) (dep_constr_B_1 t n f)) 
-  (n : nat) (b : B n) 
+Program Definition dep_elim_B (T : Type) (P : forall n : nat, B T n -> Type) (f0 : P 0 (dep_constr_B_0 T))
+  (f1 : forall (t : T) (n : nat) (f : B T n), P n f -> P (S n) (dep_constr_B_1 T t n f)) 
+  (n : nat) (b : B T n) 
 : P n b. 
 Proof.
   induction n.
-  - replace b with dep_constr_B_0 by (apply eta_dep_constr_B_0). auto.
-  - replace b with (dep_constr_B_1 (hd n b) n (tl n b)) by (apply eta_dep_constr_B_1). auto.
+  - replace b with (@dep_constr_B_0 T) by (apply eta_dep_constr_B_0). auto.
+  - replace b with (dep_constr_B_1 T (hd T n b) n (tl T n b)) by (apply eta_dep_constr_B_1). auto.
 Defined.
 
 (*
- * I expect all of our iotas to be trivial except iota_B_1:
+ * iota over A is of course trivial:
  *)
 Lemma iota_A_0 :
- forall (P : forall (n : nat), A n -> Type)
-   (f0 : P 0 dep_constr_A_0)
-   (f1 : forall (t : T) (n : nat) (f : A n), P n f -> P (S n) (dep_constr_A_1 t n f))
-   (Q : P 0 dep_constr_A_0 -> Type),
-   Q (dep_elim_A P f0 f1 0 dep_constr_A_0) ->
+ forall (T : Type) (P : forall (n : nat), A T n -> Type)
+   (f0 : P 0 (dep_constr_A_0 T))
+   (f1 : forall (t : T) (n : nat) (f : A T n), P n f -> P (S n) (dep_constr_A_1 T t n f))
+   (Q : P 0 (dep_constr_A_0 T) -> Type),
+   Q (dep_elim_A T P f0 f1 0 (dep_constr_A_0 T)) ->
    Q f0.
 Proof.
   intros. auto.
 Defined.
 
 Lemma iota_A_1 :
- forall (P : forall (n : nat), A n -> Type)
-   (f0 : P 0 dep_constr_A_0)
-   (f1 : forall (t : T) (n : nat) (f : A n), P n f -> P (S n) (dep_constr_A_1 t n f))
-   (t : T) (n : nat) (f : A n) (Q : P (S n) (dep_constr_A_1 t n f) -> Type),
-   Q (dep_elim_A P f0 f1 (S n) (dep_constr_A_1 t n f)) ->
-   Q (f1 t n f (dep_elim_A P f0 f1 n f)).
+ forall (T : Type) (P : forall (n : nat), A T n -> Type)
+   (f0 : P 0 (dep_constr_A_0 T))
+   (f1 : forall (t : T) (n : nat) (f : A T n), P n f -> P (S n) (dep_constr_A_1 T t n f))
+   (t : T) (n : nat) (f : A T n) (Q : P (S n) (dep_constr_A_1 T t n f) -> Type),
+   Q (dep_elim_A T P f0 f1 (S n) (dep_constr_A_1 T t n f)) ->
+   Q (f1 t n f (dep_elim_A T P f0 f1 n f)).
 Proof.
   intros. auto.
 Defined.
@@ -126,7 +138,7 @@ Defined.
  * Needed for iota_B_0:
  *)
 Lemma eta_refl_B_0:
-  eta_dep_constr_B_0 dep_constr_B_0 = eq_refl.
+  forall (T : Type), eta_dep_constr_B_0 T (dep_constr_B_0 T) = eq_refl.
 Proof.
   intros. unfold eta_dep_constr_B_0.
   symmetry. eapply eq_trans.
@@ -136,11 +148,11 @@ Proof.
 Defined.
 
 Lemma iota_B_0 :
- forall (P : forall (n : nat), B n -> Type)
-   (f0 : P 0 dep_constr_B_0)
-   (f1 : forall (t : T) (n : nat) (f : B n), P n f -> P (S n) (dep_constr_B_1 t n f))
-   (Q : P 0 dep_constr_B_0 -> Type),
-   Q (dep_elim_B P f0 f1 0 dep_constr_B_0) ->
+ forall (T : Type) (P : forall (n : nat), B T n -> Type)
+   (f0 : P 0 (dep_constr_B_0 T))
+   (f1 : forall (t : T) (n : nat) (f : B T n), P n f -> P (S n) (dep_constr_B_1 T t n f))
+   (Q : P 0 (dep_constr_B_0 T) -> Type),
+   Q (dep_elim_B T P f0 f1 0 (dep_constr_B_0 T)) ->
    Q f0.
 Proof.
   intros. simpl in X. rewrite eta_refl_B_0 in X. apply X.
@@ -150,8 +162,8 @@ Defined.
  * Needed for iota_B_1:
  *)
 Lemma eta_refl_B_1:
-  forall (n : nat) (t : T) (b : B n),
-    eta_dep_constr_B_1 n (dep_constr_B_1 t n b) = eq_refl.
+  forall (T : Type) (n : nat) (t : T) (b : B T n),
+    eta_dep_constr_B_1 T n (dep_constr_B_1 T t n b) = eq_refl.
 Proof.
   intros. unfold eta_dep_constr_B_1. unfold hd. unfold tl. simpl.
   symmetry. eapply eq_trans.
@@ -166,56 +178,56 @@ Proof.
 Defined.
 
 Lemma iota_B_1 :
- forall (P : forall (n : nat), B n -> Type)
-   (f0 : P 0 dep_constr_B_0)
-   (f1 : forall (t : T) (n : nat) (f : B n), P n f -> P (S n) (dep_constr_B_1 t n f))
-   (t : T) (n : nat) (b : B n) (Q : P (S n) (dep_constr_B_1 t n b) -> Type),
-   Q (dep_elim_B P f0 f1 (S n) (dep_constr_B_1 t n b)) ->
-   Q (f1 t n b (dep_elim_B P f0 f1 n b)).
+ forall (T : Type) (P : forall (n : nat), B T n -> Type)
+   (f0 : P 0 (dep_constr_B_0 T))
+   (f1 : forall (t : T) (n : nat) (f : B T n), P n f -> P (S n) (dep_constr_B_1 T t n f))
+   (t : T) (n : nat) (b : B T n) (Q : P (S n) (dep_constr_B_1 T t n b) -> Type),
+   Q (dep_elim_B T P f0 f1 (S n) (dep_constr_B_1 T t n b)) ->
+   Q (f1 t n b (dep_elim_B T P f0 f1 n b)).
 Proof.
   intros. simpl in X. unfold hd in X. unfold tl in X. simpl in X.
   rewrite eta_refl_B_1 in X. apply X.
 Defined.
 
-End Config.
-
-Section Equivalence.
+(* --- Induced equivalence --- *)
 
 (*
  * These should form their own equivalence:
  *)
-Definition f {n : nat} (a : A n) : B n :=
+Definition f (T : Type) (n : nat) (a : A T n) : B T n :=
   dep_elim_A
-    (fun n _ => B n) 
-    dep_constr_B_0
-    (fun t n b IH => dep_constr_B_1 t n IH)
+    T
+    (fun n _ => B T n) 
+    (dep_constr_B_0 T)
+    (fun t n b IH => dep_constr_B_1 T t n IH)
     n
     a.
 
-Definition g {n : nat} (b : B n) : A n :=
+Definition g (T : Type) (n : nat) (b : B T n) : A T n :=
   dep_elim_B
-    (fun n _ => A n) 
-    dep_constr_A_0
-    (fun t n b IH => dep_constr_A_1 t n IH)
+    T
+    (fun n _ => A T n) 
+    (dep_constr_A_0 T)
+    (fun t n b IH => dep_constr_A_1 T t n IH)
     n
     b.
 
 (*
  * This could be much easier, but I want to make a point of doing this algorithmically!
  *)
-Lemma section {n : nat} (a : A n) : g (f a) = a.
+Lemma section (T : Type) (n : nat) (a : A T n) : g T n (f T n a) = a.
 Proof.
-  apply dep_elim_A with (n := n) (v := a); unfold f; unfold g; intros.
+  apply dep_elim_A with (v := a); unfold f; unfold g; intros.
   - unfold dep_constr_A_0 at 1. unfold dep_constr_A_0 at 1.
-    apply (iota_B_0 (fun n _ => A n) dep_constr_A_0 (fun t n b IH => dep_constr_A_1 t n IH)).
+    apply (iota_B_0 T (fun n _ => A T n) (dep_constr_A_0 T) (fun t n b IH => dep_constr_A_1 T t n IH)).
     unfold dep_constr_B_0 at 1.
-    apply (iota_A_0 (fun n _ => B n) dep_constr_B_0 (fun t n a IH => dep_constr_B_1 t n IH)).
+    apply (iota_A_0 T (fun n _ => B T n) (dep_constr_B_0 T) (fun t n a IH => dep_constr_B_1 T t n IH)).
     reflexivity.
   - unfold dep_constr_A_1 at 1. unfold dep_constr_A_1 at 1.
-    replace (dep_constr_A_1 t n0 v) with (dep_constr_A_1 t n0 (g (f v))).
+    replace (dep_constr_A_1 T t n0 v) with (dep_constr_A_1 T t n0 (g T n0 (f T n0 v))).
     + unfold g. unfold f.
-      apply (iota_B_1 (fun n _ => A n) dep_constr_A_0 (fun t n b IH => dep_constr_A_1 t n IH) t n0).
-      apply (iota_A_1 (fun n _ => B n) dep_constr_B_0 (fun t n a IH => dep_constr_B_1 t n IH)).
+      apply (iota_B_1 T (fun n _ => A T n) (dep_constr_A_0 T) (fun t n b IH => dep_constr_A_1 T t n IH) t n0).
+      apply (iota_A_1 T (fun n _ => B T n) (dep_constr_B_0 T) (fun t n a IH => dep_constr_B_1 T t n IH)).
       reflexivity.
     + unfold g. unfold f. rewrite H. reflexivity. 
 Defined.
@@ -223,23 +235,78 @@ Defined.
 (*
  * The point being that this direction should mirror this exactly, despite iota actually mattering here.
  *)
-Lemma retraction {n : nat} (b : B n) : f (g b) = b.
+Lemma retraction (T : Type) (n : nat) (b : B T n) : f T n (g T n b) = b.
 Proof.
-  apply dep_elim_B with (n := n) (b := b); unfold f; unfold g; intros.
+  apply dep_elim_B with (b := b); unfold f; unfold g; intros.
   - unfold dep_constr_B_0 at 1. unfold dep_constr_B_0 at 1.
-    apply (iota_A_0 (fun n _ => B n) dep_constr_B_0 (fun t n a IH => dep_constr_B_1 t n IH)).
+    apply (iota_A_0 T (fun n _ => B T n) (dep_constr_B_0 T) (fun t n a IH => dep_constr_B_1 T t n IH)).
     unfold dep_constr_A_0 at 1.
-    apply (iota_B_0 (fun n _ => A n) dep_constr_A_0 (fun t n b IH => dep_constr_A_1 t n IH)).
+    apply (iota_B_0 T (fun n _ => A T n) (dep_constr_A_0 T) (fun t n b IH => dep_constr_A_1 T t n IH)).
     reflexivity.
   - unfold dep_constr_B_1 at 1. unfold dep_constr_B_1 at 1.
-    replace (dep_constr_B_1 t n0 f0) with (dep_constr_B_1 t n0 (f (g f0))).
+    replace (dep_constr_B_1 T t n0 f0) with (dep_constr_B_1 T t n0 (f T n0 (g T n0 f0))).
     + unfold f. unfold g.
-      apply (iota_A_1 (fun n _ => B n) dep_constr_B_0 (fun t n a IH => dep_constr_B_1 t n IH) t n0).
-      apply (iota_B_1 (fun n _ => A n) dep_constr_A_0 (fun t n a IH => dep_constr_A_1 t n IH)).
+      apply (iota_A_1 T (fun n _ => B T n) (dep_constr_B_0 T) (fun t n a IH => dep_constr_B_1 T t n IH) t n0).
+      apply (iota_B_1 T (fun n _ => A T n) (dep_constr_A_0 T) (fun t n a IH => dep_constr_A_1 T t n IH)).
       reflexivity.
     + unfold f. unfold g. rewrite H. reflexivity. 
 Defined.
 
-End Equivalence.
-
 (* hahaha holy shit *)
+
+(* --- Saving the equivalence --- *)
+
+Save equivalence A B { promote = f; forget = g }.
+
+(*
+ * We explicitly refer to VectorDef' here to work around the combination of
+ * universe bugs and lack of unification heuristics.
+ *)
+Configure Lift A B {
+  constrs_a = dep_constr_A_0 dep_constr_A_1;
+  constrs_b = dep_constr_B_0 dep_constr_B_1;
+  elim_a = VectorDef'.t_rect;
+  elim_b = dep_elim_B;
+  eta_a = eta_A;
+  eta_b = eta_B;
+  iota_a = iota_A_0 iota_A_1;
+  iota_b = iota_B_0 iota_B_1
+}.
+
+(* --- Repair --- *)
+
+(*
+ * Functions (provided preprocess worked) are easy, since we don't need iota.
+ * But we still need to make dep_constr_A_1 explicit because, yes, lack of unification heuristics
+ * makes things that dire, though I think that is fixable to some degree).
+ *
+ * To do that, we use "Replace Convertible" from PUMPKIN PATCH, but this won't
+ * build without PUMPKIN PATCH, so TODO expose this inside of PUMPKIN Pi.
+ * This at least proves that the problem is unification heuristics here,
+ * and that the solution is at least in some cases really not hard.
+ *)
+Module Over_A.
+
+Replace Convertible dep_constr_A_1 in VectorDef'.caseS as caseS.
+Replace Convertible dep_constr_A_1 caseS in VectorDef'.hd as hd.
+Replace Convertible caseS in VectorDef'.tl as tl.
+Replace Convertible dep_constr_A_1 in VectorDef'.const as const.
+Replace Convertible dep_constr_A_1 in VectorDef'.shiftin as shiftin.
+Replace Convertible dep_constr_A_1 in VectorDef'.take as take.
+Replace Convertible dep_constr_A_1 in VectorDef'.append as append.
+Replace Convertible dep_constr_A_1 in VectorDef'.rev_append_tail as rev_append_tail.
+Replace Convertible dep_constr_A_1 in VectorDef'.map as map.
+Replace Convertible dep_constr_A_1 in VectorDef'.fold_left as fold_left.
+Replace Convertible dep_constr_A_1 in VectorDef'.fold_right as fold_right.
+
+(* TODO universe bugs prevent us from lifting Forall, Exists, In, and so on. *)
+
+End Over_A.
+
+Set DEVOID lift type.
+Repair Module A B in Over_A as Over_B { opaque nat_rect VectorDef'.Coq_Arith_PeanoNat_Nat_nle_succ_0 VectorDef'.Coq_Arith_Le_le_S_n False_rect VectorDef'.Coq_Arith_PeanoNat_Nat_add VectorDef'.Coq_Arith_Plus_tail_plus }.
+Print Over_B.
+
+(*
+ * TODO some proofs (with iota, will be annoying), some stuff with matrices
+ *)
