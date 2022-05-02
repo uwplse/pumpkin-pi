45d43b61b21befb2ca5fa83b8ccbc6c1fa93cf5c
Cosmo Viola <cviola14580@gmail.com>
2022-05-02 02:15:29 -0500

    Use setoid typeclass in setoid equivalence.

--- plugin/coq/playground/TwoListQueue/setoid_equivalence.v
+++ plugin/coq/playground/TwoListQueue/setoid_equivalence.v
@@ -1,26 +1,18 @@
 Require Import Coq.Classes.SetoidClass.
 
 Module SetoidEquivalence.
-
-Definition is_equiv {A : Type} (eq : (A -> A -> Prop)) := True. (**replace with real condition*)
-  
-Record setoid_equiv : Type := mkEquiv
-{ A : Type
-; A_equiv : A -> A -> Prop
-; A_equiv_is_equiv : (@is_equiv A A_equiv)         
-; B : Type
-; B_equiv : B -> B -> Prop
-; B_equiv_is_equiv : (@is_equiv B B_equiv)
-; f : A -> B
+ 
+Record setoid_equiv (A : Type) `{setoida : Setoid A} (B : Type) `{setoidb : Setoid B} : Type := mkEquiv
+{f : A -> B
 ; g : B -> A
 ; f_respectful : forall (a1 a2 : A),
-    A_equiv a1 a2 -> B_equiv (f a1) (f a2)
+    equiv a1 a2 -> equiv (f a1) (f a2)
 ; g_respectful : forall (b1 b2 : B),
-    B_equiv b1 b2 -> A_equiv (g b1) (g b2)
+    equiv b1 b2 -> equiv (g b1) (g b2)
 ; section : forall (a : A),
-    A_equiv a (g (f a))
+    equiv a (g (f a))
 ; retraction : forall (b : B),
-    B_equiv b (f (g b))
-} .
+    equiv b (f (g b))
+}.
 
 End SetoidEquivalence.
