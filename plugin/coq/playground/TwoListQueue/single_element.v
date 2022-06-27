144530ea0efa5699186a3d171fd55b655258d375
Talia Ringer <talia@dependenttyp.es>
2022-06-27 13:38:39 -0700

    Organize into new/old modules for easier automation later

--- plugin/coq/playground/TwoListQueue/single_element.v
+++ plugin/coq/playground/TwoListQueue/single_element.v
@@ -1,29 +1,37 @@
+Module Old.
+
 Inductive one : Type :=
   | I : one.
-
-Inductive two : Type :=
-  | first : two
-  | second : two.
 
 Definition out_of (x : one) : nat :=
   match x with
   | I => 0
   end.
 
-Definition out_of' (x : two) : nat :=
+Definition in_to (n : nat) : one := I.
+
+Definition both (x : one) : one := I.
+
+End Old.
+
+Module New.
+
+Inductive two : Type :=
+  | first : two
+  | second : two.
+
+Definition out_of (x : two) : nat :=
   match x with
   | first => 0
   | second => 0
   end.
 
-Definition in_to (n : nat) : one := I.
+Definition in_to (n : nat) : two := first.
 
-Definition in_to' (n : nat) : two := first.
-
-Definition both (x : one) : one := I.
-
-Definition both' (x : two) : two :=
+Definition both (x : two) : two :=
   match x with
   | first => first
   | second => first
   end.
+
+End New.
